3e1d7fdcb122ac9d759922c8077c6f00521c9bc2
Jasmine <grewaljasminedeep@gmail.com>
2026-08-23 13:00:13 -0400

    ..

--- backend/src/main/java/com/example/workspace/model/Project.java
+++ backend/src/main/java/com/example/workspace/model/Project.java
@@ -7,4 +7,15 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document(collection = "projects")
 public class Project {
     @Id
+    private String id;
+    private String name;
+    private String description;
+    private List<String> memberIds;
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        
+    }
 }
