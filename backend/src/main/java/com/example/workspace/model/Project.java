051fd1f3a2b4ac90cfe337c4b1ecde67ca98111e
Jasmine <grewaljasminedeep@gmail.com>
2026-08-23 13:07:12 -0400

    ...

--- backend/src/main/java/com/example/workspace/model/Project.java
+++ backend/src/main/java/com/example/workspace/model/Project.java
@@ -16,6 +16,15 @@ public class Project {
         return id;
     }
     public void setId(String id) {
-        
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDescription() {
+        return description;
     }
 }
