0bb18949b54d5921cbda2c0b1fbca2d3f481c45e
Jasmine <grewaljasminedeep@gmail.com>
2026-08-23 15:43:58 -0400

    .

---
+++ mobile_client/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() {
+    runApp(const WorkspaceApp());
+}
+
+class WorkspaceApp extends StatelessWidget {
+    const WorkspaceApp({super.key});
+    
+}
